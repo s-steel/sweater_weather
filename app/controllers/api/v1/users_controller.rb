32607e5758fe4e32190ed7c5e8fcea199a63e643
Sean Steel <seanrsteel@gmail.com>
2021-01-17 14:55:55 -0700

    Setup create action in user controller so user is created, need to implement serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,17 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require 'pry', binding.pry
 
-    @user = User.new(user_params)
-    if @user.valid?
-      @user.save
-      flash[:success] = 'You have successfully registered!'
-      session[:user_id] = @user.id
-      redirect_to dashboard_path
-    else
-      flash[:error] = @user.errors.full_messages.to_sentence
-    end
-    # if params[:location] == ''
-    #   json_response({
-    #                   error: 'City not found, try again with proper params => ex. denver,co'
-    #                 }, status = 404)
-    # else
-    #   json_response(PhotoSerializer.background_image(PhotoFacade.image_search(params[:location])))
-    # end
+    user = User.new(user_params)
+require 'pry', binding.pry
+    user.save!
+    json_response(UserSerializer.new(user), status = 201)
+  rescue StandardError
+    json_response(UserFailedSerializer.new(user), status = 404)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
   end
 end
