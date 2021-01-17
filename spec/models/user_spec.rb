baed9a4e0d222eda5ff9f44de726c119cbb36d9b
Sean Steel <seanrsteel@gmail.com>
2021-01-17 14:01:36 -0700

    Create user model and table with migration

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it { should validate_presences_of :email }
-    it { should validate_presences_of :password_digest }
-    it { should validate_uniqueness_of :email }
-    it { should validate_uniqueness_of :api_key }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:api_key) }
+    it { should validate_presence_of(:password_confirmation) }
   end
 end
