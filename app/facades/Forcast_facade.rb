a9ff70a05028a158fa5f51b7b3293cb34da0fc60
Sean Steel <seanrsteel@gmail.com>
2021-01-16 20:31:53 -0700

    Create forcast facade with error message and tests

---
+++ app/facades/Forcast_facade.rb
@@ -0,0 +1,30 @@
+class ForcastFacade
+  class << self
+    def city_search(latitude, longitude)
+      response = OpenweatherService.city_search(latitude, longitude)
+      {
+        current_weather: WeatherCurrent.new(response),
+        daily_weather: daily_weather(response),
+        hourly_weather: hourly_weather(response)
+      }
+    rescue StandardError
+      {
+        error: 'Invalid params'
+      }
+    end
+
+    private
+
+    def daily_weather(data)
+      data[:daily].map do |day|
+        WeatherDaily.new(day)
+      end.take(5)
+    end
+
+    def hourly_weather(data)
+      data[:hourly].map do |hour|
+        WeatherHourly.new(hour)
+      end.take(8)
+    end
+  end
+end
