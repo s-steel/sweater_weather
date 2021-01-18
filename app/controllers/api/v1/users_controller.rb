c82728da812dba2b0345f1dffaed45b7447c00da
Sean Steel <seanrsteel@gmail.com>
2021-01-17 17:53:22 -0700

    Update user serializer with error messages

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-
     user = User.new(user_params)
-require 'pry', binding.pry
     user.save!
     json_response(UserSerializer.new(user), status = 201)
-  rescue StandardError
-    json_response(UserFailedSerializer.new(user), status = 404)
+  # rescue StandardError
+  #   json_response(UserSerializer.new(user), status = 404)
   end
 
   private
