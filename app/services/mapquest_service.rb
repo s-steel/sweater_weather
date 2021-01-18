d2ac6d193268f988a967d0509d55ef45ced7040f
Sean Steel <seanrsteel@gmail.com>
2021-01-18 10:53:19 -0700

    Create road trip connection and tests

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -4,17 +4,26 @@ require 'json'
 class MapquestService
   class << self
     def city_search(city)
-      city_response = conn.get('address') do |req|
+      city_response = conn.get('geocoding/v1/address') do |req|
         req.params['key'] = ENV['MAPQUEST_CONSUMER_KEY']
         req.params['location'] = city
       end
       parse_it(city_response)
     end
 
+    def road_trip(from, to)
+      trip_response = conn.get('directions/v2/route') do |req|
+        req.params['key'] = ENV['MAPQUEST_CONSUMER_KEY']
+        req.params['from'] = from
+        req.params['to'] = to
+      end
+      parse_it(trip_response)
+    end
+
     private
 
     def conn
-      Faraday.new('http://www.mapquestapi.com/geocoding/v1')
+      Faraday.new('http://www.mapquestapi.com')
     end
 
     def parse_it(data)
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -32,4 +32,23 @@ describe 'Mapquest Service' do
       expect(results[:info][:messages][0]).to eq('Illegal argument from request: Insufficient info for location')
     end
   end
+
+  describe 'Mapquest road trip' do
+    it 'query with valid params all undercase', :vcr do
+      from = 'tillamook,or'
+      to = 'denver,co'
+      results = MapquestService.road_trip(from, to)
+      route = results[:route]
+      expect(route).to have_key(:distance)
+      expect(route[:distance]).to be_a(Numeric)
+      expect(route).to have_key(:time)
+      expect(route[:time]).to be_a(Numeric)
+      expect(route[:locations][1][:latLng]).to have_key(:lng)
+      expect(route[:locations][1][:latLng]).to have_key(:lat)
+      expect(route[:locations][1][:latLng][:lng]).to be_a(Float)
+      expect(route[:locations][1][:latLng][:lng]).to eq(-104.984853)
+      expect(route[:locations][1][:latLng][:lat]).to be_a(Float)
+      expect(route[:locations][1][:latLng][:lat]).to eq(39.738453)
+    end
+  end
 end
