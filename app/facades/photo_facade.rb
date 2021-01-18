823742ba5750836a45a340c97ef89dc7032c3ce5
Sean Steel <seanrsteel@gmail.com>
2021-01-18 10:11:11 -0700

    spec

--- app/facades/photo_facade.rb
+++ app/facades/photo_facade.rb
@@ -3,10 +3,6 @@ class PhotoFacade
     def image_search(city)
       response = UpsplashService.image_search(city)[:results][0]
       Photo.new(response, city)
-    rescue StandardError
-      {
-        error: 'No photos for this location'
-      }
     end
   end
 end
