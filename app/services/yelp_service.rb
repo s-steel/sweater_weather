abdec674d695553f9145a7afcfa9119995398ab6
Sean Steel <seanrsteel@gmail.com>
2021-01-19 10:18:52 -0700

    Create yelp service connection and resonse

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -4,41 +4,24 @@ require 'json'
 class YelpService
   class << self
     def restaurant_search(location, open_at, categories)
-      
+      response = conn.get('v3/businesses/search') do |req|
+        req.params['location'] = location
+        req.params['open_at'] = open_at
+        req.params['categories'] = categories
+      end
+      parse_it(response)['businesses']
     end
-  # def self.businesses(lat, lon)
-  #   name_response = conn.get('v3/businesses/search') do |req|
-  #     req.params['latitude'] = lat
-  #     req.params['longitude'] = lon
-  #   end
-  #   parse_it(name_response)
-  # end
 
-  # def self.businesses_location(location)
-  #   name_response = conn.get('v3/businesses/search') do |req|
-  #     req.params['location'] = location
-  #   end
-  #   parse_it(name_response)
-  # end
+private
 
-  # def self.rating_sort(location)
-  #   name_response = conn.get('v3/businesses/search') do |req|
-  #     req.params['location'] = location
-  #     req.params['sort_by'] = 'rating'
-  #   end
-  #   parse_it(name_response)
-  # end
-
-    private
-    
-    def self.parse_it(data)
+    def parse_it(data)
       JSON.parse(data.body, symboloze_names: true)
     end
-    
-    def self.conn
-      yelp_key = 'Fv3iehwgkB9_XvWrJSKFFsVVy-kj3zdUeldW9lQ3JEt_9AZ-sTugSHrY5qZPORd5bL27VWpW4rsS1bv9ytUVMLX7Hv-eBn4oGXPVLgWgt6Cd3SIsDJ32sXvJU_4FYHYx'
+
+    def conn
       Faraday.new('https://api.yelp.com') do |req|
-        req.headers['Authorization'] = "Bearer #{yelp_key}"
+        req.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}"
+        req.adapter Faraday.default_adapter
       end
     end
   end
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -4,20 +4,21 @@ describe 'Yelp Service' do
   describe 'Yelp loction search' do
     it 'query with valid params', :vcr do
       location = 'denver,co'
+      open_at = 1611083495
+      categories = 'chinese'
       results = YelpService.restaurant_search(location, open_at, categories)
-      expect(results).to be_a(Hash)
-      expect(results[0]).to have_key(:id)
-      expect(results[0][:name]).to be_a(String)
-      expect(results[0][:url]).to be_a(String)
-      expect(results[0][:categories]).to be_an(Array)
-      expect(results[0][:rating]).to be_a(Float)
-      expect(results[0][:location]).to be_a(Hash)
-      expect(results[0][:location][:address1]).to be_a(String)
-      expect(results[0][:location][:address2]).to be_a(String)
-      expect(results[0][:location][:address3]).to be_a(String)
-      expect(results[0][:location][:city]).to be_a(String)
-      expect(results[0][:location][:zipcode]).to be_a(String)
-      expect(results[0][:location][:display_address]).to be_an(Array)
+      expect(results).to be_a(Array)
+      expect(results[0]).to have_key('id')
+      expect(results[0]['name']).to be_a(String)
+      expect(results[0]['url']).to be_a(String)
+      expect(results[0]['categories']).to be_an(Array)
+      expect(results[0]['rating']).to be_a(Float)
+      expect(results[0]['location']).to be_a(Hash)
+      expect(results[0]['location']['address1']).to be_a(String)
+      expect(results[0]['location']['address2']).to be_a(String)
+      expect(results[0]['location']['address3']).to be_a(String)
+      expect(results[0]['location']['city']).to be_a(String)
+      expect(results[0]['location']['display_address']).to be_an(Array)
     end
   end
 end
