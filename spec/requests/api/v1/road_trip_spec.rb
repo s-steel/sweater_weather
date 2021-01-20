7eaeb473a4710c2ab5f23f340ffcd71208e27f7e
Sean Steel <seanrsteel@gmail.com>
2021-01-19 18:53:28 -0700

    Increase testing for invalid city for a road trip

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -70,24 +70,14 @@ RSpec.describe 'Road Trip', type: :request do
         "api_key": @user.api_key.to_s
       }.to_json, headers: { 'Content-Type' => 'application/json',
                             'Accept' => 'application/json' }
-
-      expect(response).to be_successful
-      # expect(response).to have_http_status(200)
-      # road_trip_response = JSON.parse(response.body, symbolize_names: true)
-      # road_trip_data = road_trip_response[:data][:attributes]
-
-      # expect(road_trip_response[:data][:type]).to eq('roadtrip')
-      # expect(road_trip_response[:data][:id]).to eq(nil)
-      # expect(road_trip_data[:start_city]).to be_a(String)
-      # expect(road_trip_data[:end_city]).to be_a(String)
-      # expect(road_trip_data[:travel_time]).to be_a(String)
-      # expect(road_trip_data[:weather_at_eta]).to be_a(Hash)
-      # expect(road_trip_data[:weather_at_eta][:temperature]).to be_a(Float)
-      # expect(road_trip_data[:weather_at_eta][:conditions]).to be_a(String)
-
-      # expect(road_trip_data[:start_city]).to eq('Denver, CO')
-      # expect(road_trip_data[:end_city]).to eq('Pueblo, CO')
-      # expect(road_trip_data[:travel_time]).to eq('01 hours, 44 minutes')
+      expect(response).to have_http_status(200)
+      road_trip_response = JSON.parse(response.body, symbolize_names: true)
+      road_trip_data = road_trip_response[:data][:attributes]
+      expect(road_trip_data[:travel_time]).to be_a(String)
+      expect(road_trip_data[:travel_time]).to eq('impossible')
+      expect(road_trip_data[:weather_at_eta]).to be_a(Hash)
+      expect(road_trip_data[:weather_at_eta]).to_not have_key(:temperature)
+      expect(road_trip_data[:weather_at_eta]).to_not have_key(:conditions)
     end
   end
 end
